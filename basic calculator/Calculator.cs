3046b7432d61d378cf4688df0e7596021ace2f73
HeatherDa <crystal_tree@hotmail.com>
2017-04-11 08:41:03 -0500

    working on class and datavalidation.  Maybe make datavalidation class?

--- basic calculator/Calculator.cs
+++ basic calculator/Calculator.cs
@@ -10,12 +10,58 @@ namespace basic_calculator
     {
         private List<decimal> operands = new List<decimal>();
         private string operation = "";//operator
+        private decimal op1 = 0m;
+        private decimal op2 = 0m;
+        private bool prev = false;
+        private bool error = true;
 
-        public Calculator() { }
+        public Calculator(/*string eq*/)
+        {
+            /*string[] equation = eq.Split(' ');
+            if (prev)
+            {
+                ; //don't change op1 it
+            }
+            else if (!prev)
+            {
+                op1 = Decimal.Parse(equation[0]);
+                operation = equation[1];
+                op2 = Decimal.Parse(equation[2]);
+            }*/
+        }
 
-        public void EnterValue(decimal op)
+        /*public void EnterValue(decimal op)
         {
             operands.Add(op);//put value in operands list
+        }*/
+        public decimal Op1
+        {
+            get {return op1;}
+            set {op1 = value;}
+        }
+        public decimal Op1Set (string number)//this should be something for setting the value of op1, but not working?
+        {
+            if (DataValidation())
+            {
+                error = false;
+                op1 = Decimal.Parse(number);
+                return op1;//do I really need a return from this method?
+            }
+            else
+            {
+                error = true;
+                return op1;//again, return?
+            }
+            
+        }
+
+        private bool DataValidation()
+        {
+            //test for entry (must have at least 1 number?)
+            //test for decimal number
+            //test operator for operator
+            //if necessary, test for second entry
+            throw new NotImplementedException();
         }
     }
 }
--- basic calculator/Form1.cs
+++ basic calculator/Form1.cs
@@ -16,40 +16,72 @@ namespace basic_calculator
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void btn_Handler(object sender, EventArgs e)
         {
-            string btnName = "";
-            if (sender == btn1) btnName = btnName + "1";
-            else if (sender == btn2) btnName = "2";
-            else if (sender == btn3) btnName = "3";
-            else if (sender == btn4) btnName = "4";
-            else if (sender == btn5) btnName = "5";
-            else if (sender == btn6) btnName = "6";
-            else if (sender == btn7) btnName = "7";
-            else if (sender == btn8) btnName = "8";
-            else if (sender == btn9) btnName = "9";
-            else if (sender == btn0) btnName = "0";
-            else if (sender == btnDecimal) btnName = btnName + ".";
+            string operand = "";
+            if (sender == btn1) operand = operand + "1";
+            else if (sender == btn2) operand = "2";
+            else if (sender == btn3) operand = "3";
+            else if (sender == btn4) operand = "4";
+            else if (sender == btn5) operand = "5";
+            else if (sender == btn6) operand = "6";
+            else if (sender == btn7) operand = "7";
+            else if (sender == btn8) operand = "8";
+            else if (sender == btn9) operand = "9";
+            else if (sender == btn0) operand = "0";
+            else if (sender == btnDecimal) operand = operand + ".";
 
-            else if (sender == btnDivide) btnName = btnName + " / ";
-            else if (sender == btnMultiply) btnName = btnName + " * ";
-            else if (sender == btnSub) btnName = btnName + " - ";
-            else if (sender == btnAdd) btnName = btnName + " + ";
+            else if (sender == btnDivide) operand = operand + " / ";
+            else if (sender == btnMultiply) operand = operand + " * ";
+            else if (sender == btnSub) operand = operand + " - ";
+            else if (sender == btnAdd) operand = operand + " + ";
             else if (sender == btnSign) Sign(display);//toggle positive /negative
             else if (sender == btnSqrt) Sqrt(display);//calculate and display square root
             else if (sender == btnReciprocal) Reciprocal(display);//calculate and display reciprocal
             else if (sender == btnEquals) Calculate(display);//calculate results of other equations
             
-            display = display + btnName;
+            display = display + operand;
             txtDisplay.Text = display;
         }
 
         private void Calculate(string display)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
             //use string to calculate result of equation
+            Calculator c = new Calculator();
+            String[] eq = display.Split(' ');
+            if (dataValidation(eq))
+            {
+                c.Op1 = Decimal.Parse(eq[0]); //set value of first operand
+            }
+
+        }
+
+        private bool dataValidation(string[] eq)
+        {
+            //throw new NotImplementedException();
+            return isDecimal(eq) && isNotNull(eq);
+        }
+
+        private bool isNotNull(string[] eq)
+        {
+            if (eq.Length !=0) { return true; }
+            else
+            {
+                MessageBox.Show("Please enter your equation before calculating");
+                return false;
+            }//doesn't test for it being 1 or 2 or 3....
+        }
+
+        private bool isDecimal(string[] eq)//fix me
+        {
+            Decimal deq = 0;
+            if (eq.Length == 3 && Decimal.TryParse(eq[0], out deq)) { return true; }//???
+            //throw new NotImplementedException();
+            else if (eq.Length == 3 && Decimal.TryParse(eq[2], out deq)) { return false; }//???
         }
 
         private void Sign(string display)//toggle sign
